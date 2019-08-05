34b0ca63fb4bbedf2b907ccec90a74b722f09f90
github username <github email address>
2019-08-04 22:00:16 -0400

    low level error handling

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/f0daac9578589e2cd4ded22ceff26533/37.8267,-122.4233?units=us'
 
 request({url: url, json: true}, (error, response) => {
-    console.log(response.body.currently)
-    // const data = JSON.parse(response.body) //data is parsed into json with object boolean
-    const {temperature, precipProbability} = response.body.currently
-    console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
-    console.log("Summary: " + response.body.daily.data[0].summary)
+    if(error){
+        console.log("Unable to connect to weather service")
+    }
+    else if (response.body.error){
+        console.log("Unable to find location")
+    }
+    else{
+        // console.log(response.body.currently)
+        // const data = JSON.parse(response.body) //data is parsed into json with object boolean
+        const {temperature, precipProbability} = response.body.currently
+        console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
+        console.log("Summary: " + response.body.daily.data[0].summary)
+    }
+    
 })
 
 //geocoding 
@@ -16,5 +25,14 @@ request({url: url, json: true}, (error, response) => {
 const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmVpcmV5bm9zbyIsImEiOiJjanl4cHBmcGUwZXQzM2hxZWxianhycWdnIn0.PaCC5hDbkj6LrJ7tnkXH2A&limit=1'
 
 request({url: geocodingUrl, json:true}, (error,response) => {
-    console.log(response.body.features[0].center)
+    if(error){
+        console.log("Unable to connect to location services")
+    }
+    else if (response.body.features.length === 0){
+        console.log("No matching results")
+    }
+    else{
+        console.log(response.body.features[0].center)
+    }
+    
 })
