206fb270d7bb4fede7331038c79b49f8e4295a2b
github username <github email address>
2019-08-04 23:20:03 -0400

    callback refactor for forecast

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 // const url = 'https://api.darksky.net/forecast/f0daac9578589e2cd4ded22ceff26533/37.8267,-122.4233?units=us'
 
@@ -38,7 +39,21 @@ const geocode = require('./utils/geocode')
 // })
 
 
-geocode("Boston", (error,data) => {
+// geocode("Boston", (error,data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
+
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
-})
+  })
--- utils/geocode.js
+++ utils/geocode.js
@@ -19,4 +19,5 @@ const geocode = (address, callback) => {
     })
 }
 
+
 module.exports = geocode
