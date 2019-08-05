0b872211f4c58551f25530a842074463b06626e1
github username <github email address>
2019-08-04 21:39:59 -0400

    implemented geocode url

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ request({url: url, json: true}, (error, response) => {
     console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
     console.log("Summary: " + response.body.daily.data[0].summary)
 })
+
+//geocoding 
+// Address => api lat/long => weather
+
+const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmVpcmV5bm9zbyIsImEiOiJjanl4cHBmcGUwZXQzM2hxZWxianhycWdnIn0.PaCC5hDbkj6LrJ7tnkXH2A&limit=1'
+
+request({url: geocodingUrl, json:true}, (error,response) => {
+    console.log(response.body.features[0].center)
+})
